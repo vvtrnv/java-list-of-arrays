56e5c7c085ce0f67fbaa9b5d3ad7da497ace8709
Vladimir <swatik07@yandex.ru>
2022-11-05 18:08:39 +0700

    Added test for ListOfArrays class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import programUI.GUI;
+import test.TestList;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws  IOException {
+        TestList test = new TestList();
+        test.testingIntegerUserType();
+        test.testingPoint2DUserType();
 
         GUI mainFrame = new GUI();
         mainFrame.showWindow();
